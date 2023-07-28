552aa47b11c7a8c27575970d9305efa536b45c7d
samueldsiqueira <samueldsiqueira@gmail.com>
2023-07-28 12:38:27 -0300

    old version

--- samuel-landing/src/pages/index.tsx
+++ samuel-landing/src/pages/index.tsx
@@ -1,11 +1,13 @@
 import { Header } from "@/components/Header";
 import Github from "../../public/icons/github.svg";
 import Linkedin from "../../public/icons/linkedin.svg";
+import samIcon from "../../public/icons/samuelIcon.svg";
 
 
 export default function Home() {
   return(
 <Header.Root>
+  <Header.Icons icon={samIcon} link=">" />
   <Header.Content text="Samuel D' Siqueira" />
   <Header.Icons icon={Linkedin} link=">" />
   <Header.Icons icon={Github} link=">" />
